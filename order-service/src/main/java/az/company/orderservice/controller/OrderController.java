f7d492b5cda5f71c18663ffee6edaeb08fe4e135
huseynahmadov <t.huseyn96@mail.ru>
2023-02-19 14:34:24 +0400

    api gateway implemented

--- order-service/src/main/java/az/company/orderservice/controller/OrderController.java
+++ order-service/src/main/java/az/company/orderservice/controller/OrderController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/order")
+@RequestMapping("/api/orders")
 @RequiredArgsConstructor
 public class OrderController {
 
